091ee8ff2c36ed25981f5b63bf8b26bc68b34aed
Bharat Kunwar <b.kunwar@gmail.com>
2015-11-19 10:25:43 +0000

    Initial commit

---
+++ cleaner.sh
@@ -0,0 +1,4 @@
+find ./abm -iname "*.T" -exec rm -rf {} \;
+find ./abm -iname "*.EA" -exec rm -rf {} \;
+find ./abm -iname "*.pickle" -exec rm -rf {} \;
+find ./abm -iname "*.destins" -exec rm -rf {} \;
